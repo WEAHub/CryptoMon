fb13fab2af21e733487a8bba4d4701267c82c2f8
WEAHub <hruiz.dev@gmail.com>
2022-09-06 11:43:53 +0200

    Delete user added

--- src/app/modules/core/store/core-user.effects.ts
+++ src/app/modules/core/store/core-user.effects.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, exhaustMap, switchMap } from 'rxjs/operators';
-import { of } from 'rxjs';
+import { catchError, exhaustMap, map, switchMap } from 'rxjs/operators';
+import { of, tap } from 'rxjs';
 
 import * as userActions from './core-user.actions'
 import { UserService } from '../services/core-user.service';
-import { IUserSettingsResponseSuccess, IUserSettingsResponseError } from '../models/user-settings.model';
+import { IUserSettingsResponseSuccess } from '../models/user-settings.model';
 import { changedUserName } from '../../auth/store/auth.actions';
 
 @Injectable()
@@ -21,6 +21,14 @@ export class UserEffects {
 			catchError((message) => of(userActions.modifyUserError({error: message})))
 		))
 	));
+	
+	deleteUser$ = createEffect(() => this.actions$.pipe(
+		ofType(userActions.deleteUser),
+		exhaustMap(action => this.userService.deleteUser(action.data).pipe(
+			map(message => userActions.deleteUserSuccess(message)),
+			catchError(message => of(userActions.deleteUserError({ error: message })))
+		))
+	))
 
 	constructor(
 		private userService: UserService,
